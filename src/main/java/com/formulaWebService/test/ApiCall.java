a5de67b01ca409d22c881a8e5f5d95a96e94ae36
Pratik <prats.khive@gmail.com>
2015-01-18 13:01:28 -0500

    add api call for getting round details

--- src/main/java/com/formulaWebService/test/ApiCall.java
+++ src/main/java/com/formulaWebService/test/ApiCall.java
@@ -2,8 +2,10 @@ package com.formulaWebService.test;
 
 
 import retrofit.http.GET;
+import retrofit.http.Query;
 
 public interface ApiCall {
 
-
+@GET("/f1/current")
+  public void getResult(@Query("round")String round);
 }
