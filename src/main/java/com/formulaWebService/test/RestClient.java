beceb18fd20d45c2dc77e7f47a7303f1777d2ab6
Pratik <prats.khive@gmail.com>
2015-01-18 12:52:32 -0500

    change url endpoint

--- src/main/java/com/formulaWebService/test/RestClient.java
+++ src/main/java/com/formulaWebService/test/RestClient.java
@@ -8,8 +8,8 @@ import retrofit.client.OkClient;
 public class RestClient {
 
     private static ApiCall REST_CLIENT;
-    private static String ROOT =
-            "http://api.openweathermap.org/data/2.5";
+    private static String URL =
+            "http://ergast.com/api/";
 
     static {
         setupRestClient();
@@ -23,9 +23,9 @@ public class RestClient {
 
     private static void setupRestClient() {
         RestAdapter.Builder builder = new RestAdapter.Builder()
-                .setEndpoint(ROOT)
-                .setClient(new OkClient(new OkHttpClient()))
-                .builder.setLogLevel(RestAdapter.LogLevel.FULL);
+                .setEndpoint(URL)
+                .setClient(new OkClient(new OkHttpClient()));
+
 
         RestAdapter restAdapter = builder.build();
         REST_CLIENT = restAdapter.create(ApiCall.class);
