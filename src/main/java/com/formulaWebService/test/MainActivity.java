44a8da9bb85852fc1e2dba4e17c3f0cd39be63b1
Pratik <prats.khive@gmail.com>
2015-01-20 13:37:07 -0500

    add butterknife dependency, clean up activity

--- src/main/java/com/formulaWebService/test/MainActivity.java
+++ src/main/java/com/formulaWebService/test/MainActivity.java
@@ -7,57 +7,66 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import retrofit.RestClient;
 
 public class MainActivity extends Activity {
 
-    private static String TAG = "FormaulWebServiceTest";
-    Button buttonRaceDetails;
-    Button buttonDriverDetails;
-    TextView textView;
-    EditText editTextRound;
-    EditText editTextSeason;
+
     String date;
     String venue;
+
+    private static String TAG = "FormulaOneWebServiceTest";
+
+    @InjectView(R.id.buttonRaceDetails)
+    Button buttonRaceDetails;
+
+    @InjectView(R.id.textViewResponse)
+    TextView textViewResponse;
+
+    @InjectView(R.id.buttonDriverDetails)
+    Button buttonDriverDetails;
+
+    @InjectView(R.id.editTextRound)
+    EditText editTextRound;
+
+    @InjectView(R.id.editTextSeason)
+    EditText editTextSeason;
+
+    @OnClick(R.id.buttonRaceDetails)
+    void makeServiceCallForRace() {
+        makeCallToErgastWebService("race");
+    }
+
+    @OnClick(R.id.buttonDriverDetails)
+    void makeServiceCallForDrivers() {
+        makeCallToErgastWebService("driver");
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate");
         setContentView(R.layout.main);
-        buttonRaceDetails = (Button) findViewById(R.id.buttonRaceDetails);
-        buttonDriverDetails = (Button) findViewById(R.id.buttonDriverDetails);
-        textView = (TextView) findViewById(R.id.textView1);
-        editTextRound = (EditText) findViewById(R.id.editTextRound);
-        editTextSeason = (EditText) findViewById(R.id.editTextSeason);
-        buttonRaceDetails.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                makeCallToErgastWebService("race");
-            }
-        });
-        buttonDriverDetails.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                makeCallToErgastWebService("driver");
-            }
-        });
-
+        ButterKnife.inject(this);
 
     }
 
     private void makeCallToErgastWebService(String typeOfCall) {
 
-        if(typeOfCall.equals("race")){
-        ApiResponse mrData = RestClient.get().getRoundDetail(editTextRound.getText().toString());
-        date = mrData.MRData.RaceTable.Races.get(0).date;
-        venue = mrData.MRData.RaceTable.Races.get(0).raceName;
-        textView.setText(date + venue);}
-        else{
-            DriverDetails driverDetails = RestClient.get().getDriverDetails(editTextSeason.getText().toString(),"25");
+        if (typeOfCall.equals("race")) {
+            ApiResponse mrData = RestClient.get().getRoundDetail(editTextRound.getText().toString());
+            date = mrData.MRData.RaceTable.Races.get(0).date;
+            venue = mrData.MRData.RaceTable.Races.get(0).raceName;
+            textViewResponse.setText(date + venue);
+        } else {
+            DriverDetails driverDetails = RestClient.get().getDriverDetails(editTextSeason.getText().toString(), "25");
             date = driverDetails.MRData.DriverTable.Drivers.get(0).givenName;
             venue = driverDetails.MRData.DriverTable.Drivers.get(10).givenName;
-            textView.setText(date + venue);
+            textViewResponse.setText(date + venue);
         }
     }
 
