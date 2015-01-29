685e82a4c77403022b515f79ebea6a72c97d5f81
Pratik <prats.khive@gmail.com>
2015-01-29 15:05:11 -0500

    add presenter for two fragments,interface

--- src/main/java/com/formulaWebService/test/DriverDetailsFragment.java
+++ src/main/java/com/formulaWebService/test/DriverDetailsFragment.java
@@ -6,14 +6,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
-public class DriverDetailsFragment extends Fragment{
+public class DriverDetailsFragment extends Fragment implements FragmentsHelper.FragmentHelperInterface {
 
+    FragmentsHelper fragmentsHelper;
+    @InjectView(R.id.buttonDriverDetails)
+    Button buttonDriverDetails;
 
+    @InjectView(R.id.editTextSeason)
+    EditText editTextSeason;
+
+    @OnClick(R.id.buttonDriverDetails)
+    void makeServiceCallForDrivers() {
+        fragmentsHelper.makeCallToErgastWebService("driver", editTextSeason.getText().toString());
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_driver_details,container,false);
+         fragmentsHelper = new FragmentsHelper(this);
+
+
         return view;
+
+    }
+
+
+    @Override
+    public void setTextForDriverDetails(String date, String venue) {
+
+    }
+
+    @Override
+    public void setTextForRaceDetails(String givenName, String givenName1) {
 
     }
 }
---
+++ src/main/java/com/formulaWebService/test/FragmentsHelper.java
@@ -0,0 +1,63 @@
+package com.formulaWebService.test;
+
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
+import retrofit.Callback;
+import retrofit.RestClient;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+
+public class FragmentsHelper {
+
+    FragmentHelperInterface helperInterface;
+    public FragmentsHelper(FragmentHelperInterface helperInterface){
+        this.helperInterface = helperInterface;
+    }
+     void makeCallToErgastWebService(String typeOfCall,String input) {
+
+        if (typeOfCall.equals("race")) {
+            RestClient.get().getRoundDetail(input, new Callback<ApiResponse>() {
+
+
+                @Override
+                public void success(ApiResponse apiResponse, Response response) {
+                    String date = apiResponse.MRData.RaceTable.Races.get(0).date;
+                    String venue = apiResponse.MRData.RaceTable.Races.get(0).raceName;
+                    helperInterface.setTextForRaceDetails(date,venue);
+                }
+
+
+                @Override
+                public void failure(RetrofitError retrofitError) {
+                //    Toast.makeText(getActivity(), "race call failed:" + retrofitError.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+
+            });
+        } else if (typeOfCall.equals("driver")) {
+            RestClient.get().getDriverDetails(input, "25", new Callback<DriverDetails>() {
+
+                @Override
+                public void success(DriverDetails driverDetails, Response response) {
+
+                    String givenName = driverDetails.MRData.DriverTable.Drivers.get(0).givenName;
+                    String givenName1 = driverDetails.MRData.DriverTable.Drivers.get(10).givenName;
+                    helperInterface.setTextForDriverDetails(givenName,givenName1);
+                }
+
+                @Override
+                public void failure(RetrofitError retrofitError) {
+                  //  Toast.makeText(getActivity(), "driver details call failed:" + retrofitError.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+
+    public interface FragmentHelperInterface{
+        public void setTextForDriverDetails(String date, String venue);
+
+        public void setTextForRaceDetails(String givenName, String givenName1);
+    }
+}
