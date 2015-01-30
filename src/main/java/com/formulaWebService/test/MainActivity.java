438df52c2824ace21fc6cedf8bdb2bb3b1b2a776
Pratik <prats.khive@gmail.com>
2015-01-30 14:00:37 -0500

    change v4 to jar,add swipeable tabs

--- src/main/java/com/formulaWebService/test/MainActivity.java
+++ src/main/java/com/formulaWebService/test/MainActivity.java
@@ -9,46 +9,48 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 
-public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
     ActionBar actionBar;
+    ViewPager pager;
 
-    String [] tabs = {"First","Second"};
+    String[] tabs = {RaceDetailsFragment.class.getName(), DriverDetailsFragment.class.getName()};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        for(String tempTabs : tabs){
-
-            actionBar.addTab(actionBar.newTab().setText(tempTabs)
-                    .setTabListener(this));
-
-        }
-
-
-
-        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        pager = (ViewPager) findViewById(R.id.viewPager);
         pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        actionBar = getActionBar();
+        setUpActionBarTabs();
+
+
+    }
+
+    private void setUpActionBarTabs() {
+        if (actionBar != null) {
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+            for (String tempTabs : tabs) {
+                actionBar.addTab(actionBar.newTab().setText(tempTabs).setTabListener(this));
+
+            }
+        }
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        pager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
 
     }
 
     @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
 
     }
 
