8c00deb7aee3fd6bed107582990a9ab1d9f3a8bb
Pratik <prats.khive@gmail.com>
2015-02-06 14:48:01 -0500

    extract bundle to display fragment details

--- src/main/java/com/formulaWebService/test/DetailsFragment.java
+++ src/main/java/com/formulaWebService/test/DetailsFragment.java
@@ -7,20 +7,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
 public class DetailsFragment extends Fragment {
 
     @InjectView(R.id.detailsFragmentTextView)
-
-
     TextView detailsFragmentTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        String details = getArguments().getString("DRIVER_DETAILS");
-//        detailsFragmentTextView.setText(details);
-        return inflater.inflate(R.layout.fragment_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+        String details = getArguments().getString("DRIVER_DETAILS");
+        ButterKnife.inject(this, view);
+        detailsFragmentTextView.setText(details);
+        return view;
     }
 }
