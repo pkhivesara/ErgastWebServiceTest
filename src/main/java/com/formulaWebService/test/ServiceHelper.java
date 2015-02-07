94012cf417ee3ccaf2f7edb1675950856b7c9db4
Pratik <prats.khive@gmail.com>
2015-02-07 13:05:50 -0500

    add eventbus

--- src/main/java/com/formulaWebService/test/ServiceHelper.java
+++ src/main/java/com/formulaWebService/test/ServiceHelper.java
@@ -2,6 +2,7 @@ package com.formulaWebService.test;
 
 
 import datasource.DataSource;
+import de.greenrobot.event.EventBus;
 import pojo.ApiResponse;
 import pojo.DriverDetails;
 import retrofit.Callback;
@@ -12,18 +13,18 @@ import retrofit.client.Response;
 public class ServiceHelper {
 
     DataSource dataSource;
-    public ServiceHelper(DataSource dataSource){
+
+    public ServiceHelper(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
 
-    public void getRoundDetails(String input){
+    public void getRoundDetails(String input) {
         RestClient.get().getRoundDetail(input, new Callback<ApiResponse>() {
 
 
             @Override
             public void success(ApiResponse apiResponse, Response response) {
-
 
                 String date = apiResponse.MRData.RaceTable.Races.get(0).date;
                 String venue = apiResponse.MRData.RaceTable.Races.get(0).raceName;
@@ -40,13 +41,13 @@ public class ServiceHelper {
         });
     }
 
-    public void getDriverDetails(String input){
+    public void getDriverDetails(String input) {
         RestClient.get().getDriverDetails(input, "25", new Callback<DriverDetails>() {
 
             @Override
             public void success(DriverDetails driverDetails, Response response) {
-
-             //   helperInterface.setTextForDriverDetails(driverDetails);
+                dataSource.saveDriverDetails(driverDetails);
+                EventBus.getDefault().post("data saved");
             }
 
             @Override
