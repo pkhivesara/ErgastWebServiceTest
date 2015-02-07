f56f4438a70a06155e2b11565cc8ce2856963cc8
Pratik <prats.khive@gmail.com>
2015-02-07 16:00:00 -0500

    add activeAndroid statement to save values

--- src/main/java/datasource/PersistedDataSource.java
+++ src/main/java/datasource/PersistedDataSource.java
@@ -8,17 +8,19 @@ import java.util.List;
 
 
 public class PersistedDataSource implements DataSource {
+
     @Override
     public void saveDriverDetails(DriverDetails driverDetails) {
         ActiveAndroid.beginTransaction();
         DriverModel driverModel = new DriverModel(driverDetails);
         driverModel.save();
+        ActiveAndroid.setTransactionSuccessful();
         ActiveAndroid.endTransaction();
 
     }
 
     @Override
     public List<DriverModel> getDriverDetails() {
-        return new Select().from(DriverModel.class).where("id = ?", 1).execute();
+        return new Select().from(DriverModel.class).execute();
     }
 }
