97d8d3f68e5595839f3f0fc1735926f9f123dcf6
Pratik <prats.khive@gmail.com>
2015-03-31 18:23:55 -0700

    add new folder for unit test, remove unwanted xml, add hamcrest dependency

---
+++ src/test/java/presenter/DriverDetailsFragmentPresenterTest.java
@@ -0,0 +1,24 @@
+package presenter;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnit4.class)
+public class DriverDetailsFragmentPresenterTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void test() {
+        assertThat(true, is(true));
+    }
+}
